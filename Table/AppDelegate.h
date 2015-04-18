b332b8e78ad421893455091bf06dc94ca82a43ae
edutech <edutech@edutechdeMac-mini.local>
2015-04-18 12:30:27 +0800

    Initial Commit

---
+++ Table/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Table
+//
+//  Created by edutech on 15-4-18.
+//  Copyright (c) 2015年 edutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Table/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Table
+//
+//  Created by edutech on 15-4-18.
+//  Copyright (c) 2015年 edutech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
